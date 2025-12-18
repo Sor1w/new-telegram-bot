c9b490d7ea6833826789c2c08a089181d224e2f4
Sor1w <steftrofimoff@yandex.ru>
2025-12-18 19:43:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+from bot_logic import gen_pass  
+
+# Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("8250046144:AAELiO9kulZn_eZFiCSsRSCMRTCtCZkv9wM")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
